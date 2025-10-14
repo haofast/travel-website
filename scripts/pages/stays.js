6ecbd22a0595f4b71866e7140cbd4cee43dcdb9d
haofast <67030908+haofast@users.noreply.github.com>
2025-10-14 17:56:10 -0500

    update scripts

---
+++ scripts/pages/stays.js
@@ -0,0 +1,32 @@
+class Form {
+  static getMessageElement() {
+    return document.getElementById("formMessage");
+  }
+
+  static setMessageText(text) {
+    Form.getMessageElement().textContent = text;
+  }
+
+  static setFailMessage(text) {
+    Form.getMessageElement().className = "form-message form-message-failure";
+    Form.setMessageText(text);
+  }
+
+  static setSuccessMessage(text) {
+    Form.getMessageElement().className = "form-message form-message-success";
+    Form.setMessageText(text);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const flightForm = document.forms["staysForm"];
+
+  flightForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    /** insert validation code here */
+
+    Form.setSuccessMessage("Form submitted!");
+    return true;
+  });
+});
