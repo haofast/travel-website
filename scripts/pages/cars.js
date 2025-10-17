a1f2854f277ebf4c967d6f8d182b1f689bb3ad14
haofast <67030908+haofast@users.noreply.github.com>
2025-10-17 15:27:58 -0500

    fix code structure

--- scripts/pages/cars.js
+++ scripts/pages/cars.js
@@ -19,14 +19,46 @@ class Form {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const flightForm = document.forms["carForm"];
+  const carForm = document.forms["carForm"];
 
-  flightForm.addEventListener("submit", (event) => {
+  carForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    /** insert validation code here */
+    const city = carForm["city"].value.trim();
+    const carType = carForm["carType"].value.trim();
+    const checkinDate = new Date(carForm["checkinDate"].value.trim());
+    const checkoutDate = new Date(carForm["checkoutDate"].value.trim());
 
-    Form.setSuccessMessage("Form submitted!");
+    startDate = new Date("2024-09-01");
+    endDate = new Date("2024-12-01");
+
+    if (city.empty) {
+      Form.setFailMessage("Please enter a city.");
+      return false;
+    }
+
+    if (carType == "") {
+      Form.setFailMessage("Please choose a type of car.");
+      return false;
+    }
+
+    if (!(checkinDate >= startDate && checkinDate < endDate)) {
+      Form.setFailMessage("Please enter a valid check in date. Date must be on or after September 1st, 2024 and before December 1st, 2024.");
+      return false;
+    }
+
+    if (!(checkoutDate > checkinDate && checkoutDate <= endDate)) {
+      Form.setFailMessage("Please enter a valid check in date. Date must be after your check in date and before December 1st, 2024.");
+      return false;
+    }
+
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const normIn = checkinDate.toLocaleDateString('en-US', options);
+    const normOut = checkoutDate.toLocaleDateString('en-US', options);
+
+    var infoback = "Pick up city: " + city + ", Type of car: " + carType + ", Check in date: " + normIn + ", Check out date: " + normOut;
+
+    Form.setSuccessMessage(infoback);
     return true;
   });
 });
